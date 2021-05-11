3a7e4d664737d2705e5ce1c1dd0acca7f73e5696
Yan Kin Luong <yan.kin.luong@me.com>
2021-05-10 21:54:43 -0500

    10.5.2

--- scraping.py
+++ scraping.py
@@ -10,105 +10,75 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
+def mars_news(browser):
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page - searching for elements with div tag and (list_text) attribute
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Scrape Mars News
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# set up html parser
-html = browser.html
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
     news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
 
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
-# 10.3.3
-slide_elem.find('div', class_='content_title')
+    except AttributeError:
+        return None, None
 
+    return news_title, news_p
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
-# ### Featured Images
+    except AttributeError:
+        return None
 
-# 10.3.4 Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    # Use the base url to create an absolute url
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
+    return img_url
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+def mars_facts():
+    
+    try:
+      # use 'read_html" to scrape the facts table into a dataframe
+      df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        
+    except BaseException:
+        return None
+    
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
-
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
-
-
-# Find the relative image url - note the url is incomplete, only partial
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
-
-
-# 10.3.5 Scrape Mars Data: Mars fact
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
-
-
-def mars_news():
-
-   # Visit the mars nasa news site
-   url = 'https://redplanetscience.com/'
-   browser.visit(url)
-
-   # Optional delay for loading the page
-   browser.is_element_present_by_css('div.list_text', wait_time=1)
-
-   # Convert the browser html to a soup object and then quit the browser
-   html = browser.html
-   news_soup = soup(html, 'html.parser')
-
-   slide_elem = news_soup.select_one('div.list_text')
-   slide_elem.find('div', class_='content_title')
-
-   # Use the parent element to find the first <a> tag and save it as  `news_title`
-   news_title = slide_elem.find('div', class_='content_title').get_text()
-   news_title
-
-   # Use the parent element to find the paragraph text
-   news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-   news_p
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
 
 
 
-
-
-
-
-
-# Convert dataframe to html
-df.to_html()
-
-
-browser.quit()
-
-
-# 10.3.6 Export to python
 
